7ece3b72393081cda501f5f67ae30bdeda4bb09d
Athif binu <binuathif@gmail.com>
2022-05-25 23:05:10 +0530

    Btn clicked add in js

--- project2/calculator.js
+++ project2/calculator.js
@@ -1,58 +1,8 @@
-// let msg = "HELLO ATHIF";
+var screen = document.getElementById('screen');
 
 
-// result=msg.toLowerCase();
-
-// console.log(result);
-
-// let a=100;
-
-// if(a==50){
-//     console.log("true");
-// }
-// else{
-//     console.log("false");
-// }
-
-
-// let a = 50;
-
-// console.log(a != 2); not equalt operator
-
-
-//  const globelPlayer=[
-//         {
-//                name:"athif",
-//               age:12,
-//                place:"malapuram",
-//           },
-//         {
-//           name:"bilale",
-//           age:20,
-//         place:"caicurt",
-//          },
-//          {
-//            name:"bilale",
-//            age:20,
-//            place:"caicurt",
-//          }
+function Btnclick(value){
+    screen.value=value;
     
-    
-         
-//      ];
 
-
-//      const newarry=globelPlayer.filter((find)=>find.age===20);
-
-//      console.log(newarry);
-
-const hello = [10,20];
-
-
-const result = hello.reduce((totel,curentvalue)=>{
-    return totel+curentvalue;
-
-})
-
-console.log(result);
-
+}
