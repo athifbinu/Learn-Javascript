34600b237bc39ad21445b4f3b82957acb38d6dc4
Athif binu <binuathif@gmail.com>
2022-05-25 23:29:14 +0530

    to add result funtion

--- project2/calculator.js
+++ project2/calculator.js
@@ -2,7 +2,16 @@ var screen = document.getElementById('screen');
 
 
 function Btnclick(value){
-    screen.value=value;
+    screen.value+=value;
     
 
+}
+
+function ClearScreen(){
+     screen.value="";       // to clear the screen 
+}
+
+function FindResult(){
+    var result = eval(screen.value)
+    screen.value=result;
 }
