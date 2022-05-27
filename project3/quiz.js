14987e8bd8169cd61e2752e10b996881882ad2df
Athif binu <binuathif@gmail.com>
2022-05-27 11:08:36 +0530

    new html and css add

---
+++ project3/quiz.js
@@ -0,0 +1,17 @@
+const startButton = document.getElementById('start-Btn');
+startButton.addEventListener('click',startGame);
+
+
+function startGame(){
+   console.log('Strted');
+   startButton.classList.add('hide');
+}
+
+
+function setNextQustion(){
+
+}
+
+function Selectanswer(){
+
+}
