8157466519c97e03d29b82f6fc7ce987524830b9
Athif binu <binuathif@gmail.com>
2022-06-10 23:46:46 +0530

    To Deleat and Json

--- project6/app.js
+++ project6/app.js
@@ -3,6 +3,15 @@ const ItemName=document.getElementById('item-name');
 const ItemQty=document.getElementById('item-qty');       //to fetching all html in to javascript
 const list = document.getElementById('list');
 const alert=document.getElementById('alert');
+document.addEventListener('DomContentLoaded',function(){
+    const items=JSON.parse(window.localStorage.getItem('items'))
+    items.forEach(function(Item){
+        const tr=document.createElement('tr');
+        tr.innerHTML = `
+        
+        `
+    })
+})
 
 
 form.addEventListener('submit', function(event){
@@ -21,6 +30,11 @@ form.addEventListener('submit', function(event){
         `;
 
         list.appendChild(tr);
+        let items;
+        items = window.localStorage.getItem('items') ? JSON.parse(window.localStorage.getItem
+            ('items')) : [];
+        items.push({id:rand,name:ItemName.value,qty:ItemQty.value});
+        window.localStorage.setItem('items',JSON.stringify(items));
          ItemName.value = '';        //to remove the screen display on the working tyme
          ItemQty.value = '';
          alert.classList.add('alert-success');
@@ -28,9 +42,27 @@ form.addEventListener('submit', function(event){
         setTimeout(function(){
             alert.classList.remove('alert-success');
             alert.innerHTML = '';
+        },3000)                       //this funtion settimeout are allert message closing tyme
+
+
+    } else {
+        alert.classList.add('alert-warning');
+        alert.innerHTML = 'Please Add Items !';
+        setTimeout(function(){
+            alert.classList.remove('alert-warning');
+            alert.innerHTML = '';
         },3000)
-
-
     }
+
+    // Deleat
+    list.addEventListener('click',function(event){
+          if(event.target.classList.contains('btn')){
+              
+          }
+    })
+
+
+
+
 
 })
