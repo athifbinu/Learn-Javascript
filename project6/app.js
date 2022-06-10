cc9aae515c530b16859c2d4a266f81700f1d6d9d
Athif binu <binuathif@gmail.com>
2022-06-10 15:41:06 +0530

    To start javascript

---
+++ project6/app.js
@@ -0,0 +1,9 @@
+const form =document.getElementById('shop-form');
+const ItemName=document.getElementById('item-name');
+const ItemQty=document.getElementById('item-qty');
+const list = document.getElementById('list');
+
+
+form.addEventListener('submit', function(event){
+    
+})
