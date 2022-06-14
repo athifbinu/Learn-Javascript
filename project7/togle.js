8c666debe8769f307b7a57131d331c750f404026
Athif binu <binuathif@gmail.com>
2022-06-14 15:34:51 +0530

    To Develop Responsive Togle

---
+++ project7/togle.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger");        //togle button
+
+const navMenu = document.querySelector(".nav-menu");          //togle text eg ..home.gallery...
+
+
+ hamburger .addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
