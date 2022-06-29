c8fce7f9d6bdcf96c7a0e0fe2003ebbf51e1fd84
Athif binu <binuathif@gmail.com>
2022-06-29 11:58:00 +0530

    Testing

---
+++ module.js
@@ -0,0 +1,9 @@
+module.exports.hello=function(){
+    console.log("module testing")
+
+    for(i=1;i<10;i++){
+        console.log("athif")
+    }
+}
+
+module.exports.hey="Module are also sharing in variables";
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,3 @@
  //  const Minimum=Math.min(10,20,50,80,100,500,200)
+
+ 
