d7214cee56f42543e292f44dfb3de73031e77bea
Athif binu <binuathif@gmail.com>
2022-07-16 17:09:50 +0530

    express add

--- project8/index.js
+++ project8/index.js
@@ -4,6 +4,7 @@
   const username=document.getElementById("username");
   const password=document.getElementById("password");
   const logingbtn=document.querySelector(".signin");
+  
 
   logingbtn.addEventListener("click",(e)=>{
     if(username.value === "admin" && password.value === "123"){
